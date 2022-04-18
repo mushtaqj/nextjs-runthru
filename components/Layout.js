c0e43bdbbb6d615da88d8de016bb586b97f0b9f1
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-04-18 21:06:42 +0530

    add styles and logo and meta data

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,23 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  const path = router.asPath.replace("/", "") || "Home";
   return (
-    <div className="content">
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Net Ninja | {path}</title>
+      </Head>
+      <div className="content">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 };
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,21 @@
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src="/logo.png" width="128" height="77" alt="logo" />
       </div>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/about"><a>About</a></Link>
-      <Link href="/ninjas"><a>Ninja Listing</a></Link>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Ninja Listing</a>
+      </Link>
     </nav>
   );
 };
